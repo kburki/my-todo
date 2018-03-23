d1ed9992d5beb8e4bdb4cde9454d524eb84e1738
kelli burkinshaw <kburki@Kellis-MacBook-Pro-II.local>
2018-03-23 03:24:43 -0700

    - Changed assert to look for "to-do" instead of "Django". - Added new classes that inherit from unittest.TestCase - Added comments for what we intend to do - added setUp and tearDown methods (runs even if failure) - change to assertIn instead of just assert - test if run from command-line or another script

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retreive_it_later(self):
+        # Checking out home page
+        # Browse to this page
+        self.browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+        # Notice the page title mentions to-do lists
+        self.assertIn('To-Do', self.browser.title)
+
+        # Invite to enter a to-do item
+        
+        # type "Buy that book about TDD" into the text box 
+
+        # When enter the page updates and now you see
+        # 1: Buy that book about TDD" as an item in the todo list
+
+        # There is still a text box inviting you to add another.
+        # "Use python to write tests"
+
+        # The page updates again, and now shows both items on your list
+        # each generages a unique url for you with explanatory text.
+
+        # Visit the URL - your to-do list is still there.
+
+        # Satisfied, you go to sleep.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
